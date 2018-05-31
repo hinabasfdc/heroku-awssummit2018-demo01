ae81f2b31fbe3861c87e366410d8ce39cf2daca3
Hiroyuki Inaba <hinaba@salesforce.com>
2018-05-31 11:24:47 +0900

    first release

---
+++ server.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var port = process.env.PORT || 8081;
+var message = process.env.MESSAGE || '?';
+
+var message_color = 'Blue';
+
+var html_start = '<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8" /></head><body style="text-align: center;">';
+var html_body = '<img src="astro.png"></img><h1 style="color: ' + message_color + ';font-size: 6em;margin: 0px;">ようこそ!</h1><h2>AWS Summit 2018 Salesforce Booth</h2><h3>2018/05/31</h3>';
+var html_end = '</body></html>';
+
+var server = http.createServer((req, res) => {
+  var request = url.parse(req.url, true);
+  var action = request.pathname;
+
+  if (action == '/astro.png') {
+    var img = fs.readFileSync('./astro.png');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'image/png');
+    res.end(img, 'binary');
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(html_start + html_body + html_end);
+  }
+}).listen(port);
